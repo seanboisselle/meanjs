4c749d7285972640aafce9252240c9917f30e2a4
Sean Boisselle <sboisselle@ufl.edu>
2017-10-05 13:44:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,4 @@ var app = require('./config/lib/app');
 var server = app.start();
 
 // Sean was here and he stole all the cookies from the server.
+// Line 9 merge didn't show up in contributions. Line 10?
